128192f4018ba324a8bc43003e3221bfd58d6663
Dheeraj Kumar <dheerajkumar.190102@gmail.com>
2025-03-22 21:57:23 +0100

    Added tdd implementation test cases

--- labs/07_practicing_tdd/counter.py
+++ labs/07_practicing_tdd/counter.py
@@ -1,5 +1,5 @@
-from flask import Flask
 import status
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -16,3 +16,30 @@ def create_counter(name):
 
     COUNTERS[name] = 0
     return { name: COUNTERS[name] }, status.HTTP_201_CREATED
+
+
+@app.route("/counters/<name>", methods=["PUT"])
+def update_counter(name):
+    """Updates a counter"""
+    app.logger.info(f"Request to update counter: {name}")
+    global COUNTERS
+
+    COUNTERS[name] += 1
+    return { name: COUNTERS[name] }, status.HTTP_200_OK
+
+
+@app.route("/counters/<name>", methods=["GET"])
+def read_counter(name):
+    """Reads a counter"""
+    app.logger.info(f"Request to read counter: {name}")
+    global COUNTERS
+    return { name: COUNTERS[name] }, status.HTTP_200_OK
+
+
+@app.route("/counters/<name>", methods=["DELETE"])
+def delete_counter(name):
+    """Deletes a counter"""
+    app.logger.info(f"Request to delete counter: {name}")
+    global COUNTERS
+    del COUNTERS[name]
+    return {"message": ""}, status.HTTP_204_NO_CONTENT
--- labs/07_practicing_tdd/test_counter.py
+++ labs/07_practicing_tdd/test_counter.py
@@ -1,9 +1,9 @@
 """
 Test Cases for Counter Web Service
 """
-from unittest import TestCase
 import status
 from counter import app
+from unittest import TestCase
 
 class CounterTest(TestCase):
     """Test Cases for Counter Web Service"""
@@ -15,6 +15,7 @@ class CounterTest(TestCase):
         """It should create a counter"""
         result = self.client.post("/counters/foo")
         self.assertEqual(result.status_code, status.HTTP_201_CREATED)
+
         data = result.get_json()
         self.assertIn("foo", data)
         self.assertEqual(data["foo"], 0)
@@ -23,5 +24,36 @@ class CounterTest(TestCase):
         """It should return an error for duplicates"""
         result = self.client.post("/counters/bar")
         self.assertEqual(result.status_code, status.HTTP_201_CREATED)
+
         result = self.client.post("/counters/bar")
         self.assertEqual(result.status_code, status.HTTP_409_CONFLICT)
+
+    def test_increment_count(self):
+        """It should increment the counter"""
+        result = self.client.post("/counters/baz")
+        self.assertEqual(result.status_code, status.HTTP_201_CREATED)
+        data = result.get_json()
+        base = data["baz"]
+
+        result = self.client.put("/counters/baz")
+        self.assertEqual(result.status_code, status.HTTP_200_OK)
+        data = result.get_json()
+        self.assertEqual(data["baz"], base + 1)
+
+    def test_read_counter(self):
+        """It should read the counter"""
+        result = self.client.post("/counters/qux")
+        self.assertEqual(result.status_code, status.HTTP_201_CREATED)
+
+        result = self.client.get("/counters/qux")
+        self.assertEqual(result.status_code, status.HTTP_200_OK)
+        data = result.get_json()
+        self.assertEqual(data["qux"], 0)
+
+    def test_delete_counter(self):
+        """It should delete the counter"""
+        result = self.client.post("/counters/quux")
+        self.assertEqual(result.status_code, status.HTTP_201_CREATED)
+
+        result = self.client.delete("/counters/quux")
+        self.assertEqual(result.status_code, status.HTTP_204_NO_CONTENT)
