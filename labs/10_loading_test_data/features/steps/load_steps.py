a1565c1ff65d8feb90e08c5b49b34a0020ee472c
Dheeraj Kumar <dheerajkumar.190102@gmail.com>
2025-03-23 23:24:43 +0100

    Added load steps

--- labs/10_loading_test_data/features/steps/load_steps.py
+++ labs/10_loading_test_data/features/steps/load_steps.py
@@ -9,5 +9,27 @@ For information on Waiting until elements are present in the HTML see:
 import requests
 from behave import given
 
-# Load data here
+@given('the following pets')
+def step_impl(context):
+    """Load the pets into the database"""
 
+    # Delete existing pets
+    response = requests.get(f"{context.base_url}/pets")
+    assert response.status_code == 200
+
+    for pet in response.json():
+        response_to_delete = requests.delete(f"{context.base_url}/pets/{pet['id']}")
+        assert response_to_delete.status_code == 204
+
+    # Load the new pets
+    for row in context.table:
+        payload = {
+            'name': row['name'],
+            'category': row['category'],
+            'available': row['available'],
+            'gender': row['gender'],
+            'birthdate': row['birthdate']
+        }
+
+    response = requests.post(f"{context.base_url}/pets", json=payload)
+    assert context.response.status_code == 201
