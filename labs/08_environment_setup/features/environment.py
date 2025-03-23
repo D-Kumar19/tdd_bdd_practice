84e102d63da1c4b98e768faffc372d36850efa15
Dheeraj Kumar <dheerajkumar.190102@gmail.com>
2025-03-23 22:35:53 +0100

    Added environment vars

--- labs/08_environment_setup/features/environment.py
+++ labs/08_environment_setup/features/environment.py
@@ -4,9 +4,13 @@ Environment for Behave Testing
 from os import getenv
 from selenium import webdriver
 
+WAIT_SECONDS = int(getenv('WAIT_SECONDS', 60))
+BASEURL = getenv('BASE_URL', 'http://localhost:8080')
 
 def before_all(context):
     """ Executed once before all tests """
+    context.wait_seconds = WAIT_SECONDS
+    context.base_url = BASEURL
 
 
 def after_all(context):
