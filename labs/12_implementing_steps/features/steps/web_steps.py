4809a5298fa0845b8ca5ed5099f1bc93dd881436
Dheeraj Kumar <dheerajkumar.190102@gmail.com>
2025-03-24 08:12:01 +0100

    Implemented test cases using gherkin

--- labs/12_implementing_steps/features/steps/web_steps.py
+++ labs/12_implementing_steps/features/steps/web_steps.py
@@ -12,34 +12,36 @@ from selenium.webdriver.common.by import By
 
 @given('I am on the "Home Page"')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Given I am on the "Home Page"')
+    context.response = context.driver.get(context.base_url)
 
 
-@when('I set the "Category" to "dog"')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: When I set the "Category" to "dog"')
+@when('I set the "Category" to "{pet_name}"')
+def step_impl(context, pet_name):
+    element = context.driver.find_element(By.ID, 'pet_category')
+    element.clear()
+    element.send_keys(pet_name)
 
 
 @when('I click the "Search" button')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: When I click the "Search" button')
+    element = context.driver.find_element(By.ID, 'search-btn')
+    element.click()
 
 
-@then('I should see the message "Success"')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: Then I should see the message "Success"')
+@then('I should see the message "{message}"')
+def step_impl(context, message):
+    message = message.lower().replace(' ', '_')
+    element = context.driver.find_element(By.ID, 'flash_message')
+    assert message in element.text
 
 
-@then('I should see "Fido" in the results')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: Then I should see "Fido" in the results')
+@then('I should see "{pet_name}" in the results')
+def step_impl(context, pet_name):
+    element = context.driver.find_element(By.ID, 'search_results')
+    assert pet_name in element.text
 
 
-@then('I should not see "Kitty" in the results')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: Then I should not see "Kitty" in the results')
-
-
-@then('I should not see "Leo" in the results')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: Then I should not see "Leo" in the results')
+@then('I should not see "{pet_name}" in the results')
+def step_impl(context, pet_name):
+    element = context.driver.find_element(By.ID, 'search_results')
+    assert pet_name not in element.text
